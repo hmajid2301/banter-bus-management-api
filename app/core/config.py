4b0cdc264f334297c2da0d2530e1fa2928168a39
Haseeb Majid <hello@haseebmajid.dev>
2022-01-04 00:44:00 +0000

    refactor: remove auth0
    Remove remaining auth0 references in code.

--- app/core/config.py
+++ app/core/config.py
@@ -4,9 +4,6 @@ from omnibus.config.settings import OmnibusSettings
 
 
 class Settings(OmnibusSettings):
-    AUTH0_DOMAIN: str
-    AUTH0_CUSTOM_API: str
-
     class Config:
         env_prefix = "BANTER_BUS_MANAGEMENT_API_"
         env_file = ".env"
